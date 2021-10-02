5a8c5a62e3975c5e06ee28853f757f91d894c98c
StatistikVolker <holzendorf@gmx.eu>
2021-10-02 16:34:40 +0200

    benötigte Pakete und funktionen

---
+++ 010_load packages.r
@@ -0,0 +1,28 @@
+## 010_load packages.r
+
+library(tidyverse) # R-Welt
+library(readr) # ladt csv-dateien
+library(rgdal) # lädt Shapefiles
+library(sp) # für Geoaten
+library(sf) # simple feature Geodaten
+library(leaflet) # für Karten
+library(cartogram) # Verzerrte Karten
+#library(mapview) # zum Abspeichern von statischen Karten
+library(htmlwidgets) # zum Abspeichern von dynamischen Karten
+
+library(plotly) # Interactive Plots
+library(rgeos) 
+library(scales)
+
+#library(Rdatacheck)
+
+
+# Muss in Paket Rdatacheck integriert werden
+mkr.formatwithdigits<-function(val,digits=3){
+  fval<-rep(NA,length(val))
+  for(i in (1:length(val))){
+    fval[i]<-formatC(val[i],digits=digits,format="f")
+  }
+  return(fval)
+}
+
