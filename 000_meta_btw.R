09bfdf2890762c97a5722a81869e7c255740ecbb
StatistikVolker <holzendorf@gmx.eu>
2021-10-06 06:33:50 +0200

    Anpassungen für Jenadaten

--- 000_meta_btw.R
+++ 000_meta_btw.R
@@ -6,9 +6,14 @@
 source("010_load packages.r")
 
 # Wahldaten einlesen ------------------------------------------------------
-
+# Wahlkreise Deutschlandweit
 source("011_create_btwdata.R")
 source("012_load_wkshapes.R")
+
+# Wahlbezirke Jena
+source("111_create_btwJena.R")
+source("112_load_wbzJhapes.R")
+
 
 # Karte -----------------------------------------------------------
 
@@ -16,10 +21,10 @@ source("012_load_wkshapes.R")
 # Farben der Parteien
 # ---------------------------------------------------------------------------
 
-print(levels(BTW21$Gruppenname_1))
+print(levels(BTW21J$Gruppenname_1))
 pal <- colorFactor(
   palette = c('darkblue', 'black', 'grey25', 'purple', 'forestgreen',"red"),
-  domain = BTW21$Gruppenname_1
+  domain = BTW21J$Gruppenname_1
 )
 #pal(levels(BTW21$Gruppenname_1))
 
@@ -27,16 +32,22 @@ pal <- colorFactor(
 # Unverzerrte Wahlkreiskarten
 # ---------------------------------------------------------------------------
 
-# Erstimmenergebnisse
-m<-leaflet() %>% #addTiles() %>% 
-  addPolygons(data=btw1st_spdf, fillColor = ~pal(Gruppenname_1), weight=2,fillOpacity = ~(Prozent_Diff/max(Prozent_Diff))+0.25)
-m
-saveWidget(m, file="maps/map_btw1st.html")
-# Zweitstimmenergebnisse
+## Erstimmenergebnisse
+#m<-leaflet() %>% #addTiles() %>% 
+#  addPolygons(data=btw1st_spdf, fillColor = ~pal(Gruppenname_1), weight=2,fillOpacity = ~(Prozent_Diff/max(Prozent_Diff))+0.25)
+#m
+#saveWidget(m, file="maps/map_btw1st.html")
+## Zweitstimmenergebnisse
+#m<-leaflet() %>% #addTiles() %>% 
+#  addPolygons(data=btw2st_spdf, fillColor = ~pal(Gruppenname_1), weight=2,fillOpacity = ~(Prozent_Diff/max(Prozent_Diff))+0.25)
+#m
+#saveWidget(m, file="maps/map_btw2st.html")
+
+names(btw2st_spdf)
 m<-leaflet() %>% #addTiles() %>% 
   addPolygons(data=btw2st_spdf, fillColor = ~pal(Gruppenname_1), weight=2,fillOpacity = ~(Prozent_Diff/max(Prozent_Diff))+0.25)
 m
-saveWidget(m, file="maps/map_btw2st.html")
+
 
 # ---------------------------------------------------------------------------
 # Verzerrte Wahlkreiskarten: 
@@ -45,19 +56,22 @@ saveWidget(m, file="maps/map_btw2st.html")
 # Zweitstimmen: 
 
 source("mkr_btw21cartogram.r")
+source("mkr_btw21cartogram_Jena.r")
+
 
 mkr.btw21_cartogram(
-party = "FDP", #als Vector if Union
-prtycol = "yellow",
-vote = "Zweitstimme" #,# or "Erststimme" # first letter is enough
-#cartoweight = "Anzahl" ,
-#itermax = 10, #Default 10
-#colvar = "Prozent",
-#legendlab = "Zweitstimmenergebnis [%]",
-#colgrps = c(o,5,10,15,20,25,30,35,40,45) , # Grenzen der Farbabstufungen, start with "0"
-#collabs = c("bis 5%","5-10%","10-15%","15-20%","20-25%","25-30%","30-35%","35-40%","40-45%","über 45%"),
-#alphastart = 0.25,
-#alphaend = 1
+
+  party = "FDP", #als Vector if Union
+  prtycol = "yellow",
+  vote = "Zweitstimme" #,# or "Erststimme" # first letter is enough
+  #cartoweight = "Anzahl" ,
+  #itermax = 10, #Default 10
+  #colvar = "Prozent",
+  #legendlab = "Zweitstimmenergebnis [%]",
+  #colgrps = c(o,5,10,15,20,25,30,35,40,45) , # Grenzen der Farbabstufungen, start with "0"
+  #collabs = c("bis 5%","5-10%","10-15%","15-20%","20-25%","25-30%","30-35%","35-40%","40-45%","über 45%"),
+  #alphastart = 0.25,
+  #alphaend = 1
 )
 
 mkr.btw21_cartogram(
