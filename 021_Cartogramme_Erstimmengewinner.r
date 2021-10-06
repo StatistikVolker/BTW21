f96bc7b3833b3bc8be2dcba1041bea578cd5585f
StatistikVolker <holzendorf@gmx.eu>
2021-10-06 06:34:08 +0200

    Anpassungen Jena

--- 021_Cartogramme_Erstimmengewinner.r
+++ 021_Cartogramme_Erstimmengewinner.r
@@ -53,3 +53,33 @@ m#apshot(m, file = "BTW21_Erst_cartogram.png")
 
 
 
+# --------------------------------------------------------------------------------
+# JENA
+# --------------------------------------------------------------------------------
+btw2st_spdf2<-st_as_sf(btw2st_spdf) #as simple feature
+# neue Coordinaten
+btw2st_spdf2 <- st_transform(btw2st_spdf2, coords = c("lon", "lat"), 
+                             crs = "+init=epsg:3395", 
+                             agr = "constant")
+# Erststimmen: Anzahl Stimmen Erster ist Verzerrungsfaktor
+btw1st_AS_conto <- cartogram_cont(btw2st_spdf2, "Anzahl_1",itermax = 10)
+btw1st_AS_cont<- st_transform(btw1st_AS_conto, 4326) %>%
+  mutate(alpha =ifelse((Prozent_Diff/max(Prozent_Diff)+0.25)<1, Prozent_Diff/max(Prozent_Diff)+0.25,1),
+         palcol = pal(Gruppenname_1)) #%>%
+#st_cast("MULTIPOLYGON") %>%
+#st_cast("POLYGON",do_group = FALSE) %>%
+#filter(Gruppenname_1 == "DIE LINKE")
+#filter(substr(Gebietsname,1,7) == "Leipzig")# ("+proj=longlat +datum=WGS84")
+#filter(WKR_NR == 153)
+summary(btw1st_AS_cont$alpha)
+
+btw1st_AS_cont <- as_Spatial(btw1st_AS_cont)#,cast = FALSE)
+class(btw1st_AS_cont)
+m<-leaflet() %>% #addTiles() %>% 
+  addPolygons(data=btw1st_AS_cont, fillColor = ~palcol, weight=1,fillOpacity = ~alpha)
+m
+saveWidget(m, file="Jenamaps/BTW21_Zweit_nachWBZ_cartogram.html")
+mapshot(m, file = "Jenamaps/BTW21_Zweit_nachWBZ_cartogram.png")
+
+
+
